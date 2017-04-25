a38affc662de35524823530f54b028cb1b0305b0
Helenajh <helena.jianghe@gmail.com>
2017-04-25 14:02:05 -0500

    Add files via upload

--- app.r
+++ app.r
@@ -7,48 +7,70 @@ library(ggplot2)
 library(rvest)
 
 
-ames <- read.csv("Ames_housing.csv", header = T)
+amesHousingDat <- read.csv("Ames_housing.csv", header = T)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  titlePanel("House Sales Search"),
+  titlePanel("House Sale History Search"),
   
   sidebarLayout(
     sidebarPanel(
     
-      selectInput("style", label = h3("Select Housing Style"), 
-                  choices = unique(ames$Style),
-                  selected = "Two story")),
-    
+      selectInput("Type", label = h3("Select Building Type"), 
+                  choices = unique(amesHousingDat$Occupancy),
+                  selected = "Single-family detached (includes detached townhouses)"),
+      
+      checkboxGroupInput("Bedrooms", label = h3("Select number of bedrooms"), 
+                         choices = list("1 bed(orange)" = 1, "2 beds (blue)" = 2, "3 beds (red)" = 3, 
+                                        "4 beds (green)" = 4, "5 beds (purple)" = 5, "6 beds (yellow)" = 6,
+                                        "7 beds (pink)" = 7, "8 beds (brown)" = 8),
+                         selected = 3), 
+      
+      sliderInput("Year", label = h3("Select Year Built"), min = 1890, 
+                  max = 2016, value = c(1980,1995))
+    ),
       
     # Show a plot of the generated distribution
     mainPanel(
-      plotlyOutput("plot"),
-      textOutput("text"),
-      tableOutput("table")
+      plotlyOutput("salePriceAreaBed")
+
     )
   )
 )
 
 
-# Define server logic required to draw a histogram
+
+
 server <- function(input, output) {
 
-  ames_subset <- reactive(
-    ames %>% filter(Style == input$style))
+  ames_tab2_subset <- reactive(
+    amesHousingDat %>% filter(Occupancy == input$Type, Bedrooms %in% c(input$Bedrooms), 
+                            Year.Built > input$Year[1], Year.Built < input$Year[2])
+    )
   
-  output$plot <- renderPlotly(
-    ames_subset() %>% ggplot(aes(y=Sale.Price/1000, x= Total.Living.Area, 
-                                 colour = as.factor(Bedrooms))) + geom_point()
-                      +theme_classic() + xlab("Total Living Area") + ylab("\nSale Price(k)\n")
-                    +scale_colour_discrete(name  ="Bedroom")                   
-                   + theme(legend.position="bottom")
-  )
-  
+  output$salePriceAreaBed <- renderPlotly({
+    ames_tab2_subset() %>% 
+      ggplot(aes(y = Sale.Price/1000, x = Total.Living.Area, 
+                 colour = as.factor(Bedrooms),
+                 shape = factor(Style),
+                 alpha = 0.5
+      )
+      ) + 
+      geom_point() + 
+      theme_classic() + 
+      xlab("Total Living Area (sq ft)") + 
+      ylab("\n Sale Price(k) \n") + 
+      # scale_colour_discrete(name  = "Bedroom") + 
+      theme(legend.position = "none")
+    #    theme(legend.position = "bottom") 
+  })
 }
 
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
 
